ec3a75dcf3e34cf72c11a6caed50a9c4d3990638
csfx-py <csfxshreyas@gmail.com>
2021-09-07 20:27:32 +0530

    added change password route

--- src/routes/Auth/AuthManager.js
+++ src/routes/Auth/AuthManager.js
@@ -62,4 +62,27 @@ router.post("/login", async (req, res) => {
   res.send(createToken(user, process.env.ACCESS_TOKEN_SEC, "3d"));
 });
 
+router.post("/changePassword", async (req, res) => {
+  const { name, oldPassword, newPassword } = req.body;
+
+  //   check exists
+  const user = await User.findOne({ name });
+  if (!user) return res.status(401).json("User not found");
+
+  // authenticate
+  const authenticaed = await bcrypt.compare(oldPassword, user.password);
+  if (!authenticaed) return res.status(401).send("Incorrect password");
+
+  const salt = await bcrypt.genSalt(10);
+  const hashPass = await bcrypt.hash(newPassword, salt);
+
+  try {
+    user.password = hashPass;
+    const savedUser = await user.save();
+    res.status(200).send(createToken(user, process.env.ACCESS_TOKEN_SEC, "3d"));
+  } catch (err) {
+    res.status(500).send(err || "Internal Server error");
+  }
+});
+
 module.exports = router;
