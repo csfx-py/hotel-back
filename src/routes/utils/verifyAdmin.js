18b6d2aa89f67896b5240bd0c4906c98b0f07658
csfx-py <csfxshreyas@gmail.com>
2021-09-08 04:17:03 +0530

    added verifyAdmin utility

---
+++ src/routes/utils/verifyAdmin.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  const token = req.header("auth");
+  if (!token) return res.status(401).send("Unauthorized");
+
+  try {
+    const payload = jwt.verify(token, process.env.ACCESS_TOKEN_SEC);
+    if(!payload) return res.status(401).send("Unauthorized");
+    if(!payload.admin) return res.status(401).send("Unauthorized");
+
+    next();
+  } catch (err) {
+    res.status(401).send("Unauthorized. Error: " + err);
+  }
+};
